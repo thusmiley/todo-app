30c51d876f35e78ad896cb980670a281466bd14f
Thu Dang <>
2023-11-23 18:36:32 -0600

    wip 1123

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
         <meta property="og:image" content="/images/desktop-preview.jpg" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
 
--- app/page.js
+++ app/page.js
@@ -6,6 +6,11 @@ import Image from "next/image";
 
 export default function Home() {
   const [isDark, setIsDark] = useState("dark");
+  const [newInput, setNewInput] = useState("");
+  const [countId, setCountId] = useState(0);
+  const [todos, setTodos] = useState([]);
+  const [isCompleted, setIsCompleted] = useState(false);
+
   useEffect(() => {
     if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
       setIsDark("dark");
@@ -13,40 +18,75 @@ export default function Home() {
       setIsDark("light");
     }
   });
+  // let todoList = ["Let's get started!"];
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setCountId(countId + 1);
+    console.log(countId);
+    if (newInput) {
+      const newTodo = {
+        id: countId,
+        description: newInput.trim(),
+        completed: false,
+      };
+      setTodos([newTodo, ...todos]);
+      setNewInput("");
+    }
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between mt-[-88px] mb-[72px]">
-      <form action="" className="container px-6 mx-auto relative">
-        <div className="bg-white py-[14px] px-5 rounded-[5px] mb-4">
-          <div className="w-5 h-5 rounded-full border-[1px] border-gradient-bg bg-gradient-bg" />
-          <input type="text" name="" id="" placeholder="Create a new todo..." />
+      <form action="" className="container px-6 mx-auto relative" onSubmit={handleSubmit}>
+        <div className="bg-white py-[14px] px-5 rounded-[5px] mb-4 flex items-center justify-start">
+          <div className="w-[21px] h-5 rounded-full border-[1px] border-[#E3E4F1] mr-3" />
+          <input
+            type="text"
+            name="newInput"
+            id="newInput"
+            placeholder="Create a new todo..."
+            className=""
+            value={newInput}
+            onChange={(e) => {
+              setNewInput(e.target.value);
+            }}
+          />
         </div>
 
         <div className="bg-white py-[14px] px-5 rounded-[5px] w-full overflow-hidden shadow-md">
           <div className="relative">
-            <div className="flex justify-between items-center">
-              <div>
-                <input type="radio" name="" id="" />
-                <label htmlFor="">Jog around the park 3x</label>
-              </div>
-              <Image src={iconCross} width={12} height={12} className="" />
-            </div>
-
+            {todos.map((todo) => {
+              <div key={todo.id} className="flex justify-between items-center group cursor-pointer">
+                <div className="cursor-pointer flex items-center">
+                  <div
+                    className={`${
+                      isCompleted ? "opacity-100" : "opacity-20"
+                    } w-5 h-5 gradient-bg rounded-full mr-3  flex justify-center items-center group-hover:opacity-100 transition-opacity duration-300 ease-in-out`}
+                  >
+                    <input type="checkbox" name="todo" id={`todo-${todo.id}`} className="cursor-pointer" onChange={() => setIsCompleted(!isCompleted)} />
+                  </div>
+                  <label htmlFor="todo" className="cursor-pointer text-[12px] tracking-[-.17px] text-dark49 truncate">
+                    {todo.description}
+                  </label>
+                </div>
+                <Image src={iconCross} width={12} height={12} className="hidden group-hover:block" />
+              </div>;
+            })}
             <div className="w-[120%] border-[1px] border-[#E3E4F1] absolute bottom-[-14px] left-[-10%]" />
           </div>
 
           <div className="mt-8 flex justify-between items-center">
-            <p>5 items left</p>
-            <div className="flex justify-between items-center absolute bottom-[70px]">
+            <p className="text-grey94 text-[12px] tracking-[-.17px]">5 items left</p>
+            <div className="flex justify-center items-center absolute bottom-[75px] font-bold text-grey94 text-[14px] tracking-[-.19px] left-0 right-0 mx-auto space-x-[18px]">
               <p>All</p>
               <p>Active</p>
               <p>Completed</p>
             </div>
-            <p>Clear Completed</p>
+            <p className="text-grey94 text-[12px] tracking-[-.17px]">Clear Completed</p>
           </div>
         </div>
         <div className="h-[48px] bg-white rounded-[5px] mt-4 shadow-md" />
 
-        <p className="text-grey94 mt-10 text-center">Drag and drop to reorder list</p>
+        <p className="text-grey94 mt-10 text-center text-[14px] tracking-[-.19px]">Drag and drop to reorder list</p>
       </form>
     </main>
   );
